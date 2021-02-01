64728a8095916b6afe683640a54b5744269036b9
vagrant <risa.mkwork314@gail.com>
2021-02-01 14:33:04 +0000

    devise:admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :admins
+  root to: "books#index"
+  get 'books/show'
+  get 'books/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
