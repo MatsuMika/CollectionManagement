3e191c8f9182ff6cbd53ada5f5e357aae5f53472
vagrant <risa.mkwork314@gmail.com>
2021-02-06 14:55:04 +0000

    [Add]header,top

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+
+	protected
+
+	# ログイン後のリダイレクト先
+  def after_sign_in_path_for(resource)
+		books_path
+  end
+
+	# ログアウト後のリダイレクト先
+  def after_sign_out_path_for(resource)
+		root_path
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: "books#index"
+	root to: 'homes#top'
 
 	resources :categories, only: [:index, :create, :edit, :update, :destroy]
-	resources :books, only: [:new, :create, :show, :create, :edit, :update, :destroy]
+	resources :books
 
 end
