6f4a79304211b8ee98cdff5a80a9ada07fba89de
vagrant <risa.mkwork314@gmail.com>
2021-02-17 14:54:23 +0000

    [Add]controller:lends

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+  get 'lends/new'
+  get 'lends/index'
+  get 'lends/show'
   devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
---
+++ test/controllers/lends_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class LendsControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get lends_new_url
+    assert_response :success
+  end
+
+  test "should get index" do
+    get lends_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get lends_show_url
+    assert_response :success
+  end
+
+end
